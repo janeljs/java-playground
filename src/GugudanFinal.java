b4cd847c7da8c575bddd6e03e693ed1f46d923cf
Jisun Lim <jisunlim818@gmail.com>
2020-12-03 05:33:45 +0900

    Gugudan final 예제

---
+++ src/GugudanFinal.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class GugudanFinal {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String[] data = sc.nextLine().split(" ");
+		int n = Integer.parseInt(data[0]);
+		int m = Integer.parseInt(data[1]);
+		
+		for (int j = 1; j <= m; j++) {
+			System.out.println(n + "단:" + n + "x" + j + "=" + n * j);
+		}
+	}
+}
