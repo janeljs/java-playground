cc6cd17026665d5bf734a0db8177914422cd3fb8
Jisun Lim <jisunlim818@gmail.com>
2020-12-03 04:39:52 +0900

    Update Gugudan

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -5,7 +5,13 @@ public class Gugudan {
 		System.out.println("구구단 중 출력할 단은? : ");
 		Scanner sc = new Scanner(System.in);
 		int i = sc.nextInt();
-
+		System.out.println("사용자가 입력한 값: " + i);
+		if (i <2 || i>9) {
+			System.out.println("2이상, 9이하의 값만 입력할 수 있습니다.");
+			System.out.println("구구단 중 출력할 단은? : ");
+//			sc = new Scanner(System.in);
+			i = sc.nextInt();
+		}
 		for (int j = 1; j < 10; j++) {
 			System.out.println(i + "단:" + i + "x" + j + "=" + i * j);
 		}
