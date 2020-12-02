331edbf95e91f6f407dbe26ecb8217e18d3a1684
Jisun Lim <jisunlim818@gmail.com>
2020-12-03 07:30:59 +0900

    update Calendar

--- Calendar/janeljs/calendar/Calendar.java
+++ Calendar/janeljs/calendar/Calendar.java
@@ -1,6 +1,26 @@
 package janeljs.calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
+	public static void getLastDay(int month) {
+		System.out.println("달을 입력하세요.");
+		Scanner sc = new Scanner(System.in);
+		month = sc.nextInt();
+		int lastday = 0;
+		if (month !=2) {
+			if (month%2 == 0)
+				lastday = 30;
+			else {
+				lastday = 31;
+			}
+		}
+		else {
+			lastday = 28;
+		}
+		System.out.println(month + "월은 " + lastday + "일까지 있습니다.");
+	}
+
 	public static void main(String[] args) {
 		System.out.println("일 월 화 수 목 금 토");
 		System.out.println("--------------------");
@@ -8,6 +28,7 @@ public class Calendar {
 		System.out.println(" 8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
-	
+		System.out.println();
+		getLastDay(3);
 	}
 }
