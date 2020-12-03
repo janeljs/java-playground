a449c698b1f525928d7d73cc3d6737d06a486cfd
Jisun Lim <jisunlim818@gmail.com>
2020-12-03 19:00:17 +0900

    update Calendar.java

--- Calendar/janeljs/calendar/Calendar.java
+++ Calendar/janeljs/calendar/Calendar.java
@@ -4,15 +4,11 @@ import java.util.Scanner;
 
 public class Calendar {
 
-	public static void main(String[] args) {
+	
 
-		Calendar cal = new Calendar();
-		cal.getLastDay(3);
-		cal.sampleCalendar(3);
-	}
-
-	public void sampleCalendar(int month) {
-		System.out.println(" 일 월 화 수 목 금 토");
+	public static void printCalendar(int year, int month) {
+		System.out.printf("    <<%4d년 %d월>>\n", year, month);
+		System.out.println("SU MO TU WE TH FR SA");
 		System.out.println("--------------------");
 		
 		int temp = 0;
@@ -42,48 +38,17 @@ public class Calendar {
 		}
 	}
 
-	public void getLastDay(int month) {
-		String PROMPT = "cal> ";
-		System.out.println("반복 횟수를 입력하세요.");
-		System.out.print(PROMPT);
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int[] monthArr = new int[N];
-		int[] lastdayArr = new int[N];
-		System.out.println("달을 입력하세요.");
-		for (int i = 0; i < N; i++) {
-			System.out.print(PROMPT);
-			monthArr[i] = sc.nextInt();
-
-			if (monthArr[i] > 12) {
-				System.out.println("유효한 값이 아닙니다.");
-				monthArr[i] = sc.nextInt();
+	public static int getLastDay(int month) {
+		int lastday = 0;
+		if (month != 2) {
+			if (month % 2 == 0)
+				lastday = 30;
+			else {
+				lastday = 31;
 			}
-			if (monthArr[i] == -1) {
-				System.out.println("Bye~");
-				break;
-			}
-
-			if (monthArr[i] != 2) {
-				if (monthArr[i] % 2 == 0)
-					lastdayArr[i] = 30;
-				else {
-					lastdayArr[i] = 31;
-				}
-			} else {
-				lastdayArr[i] = 28;
-			}
-
+		} else {
+			lastday = 28;
 		}
-
-		for (int i = 0; i < N; i++) {
-			if (monthArr[i] == -1) {
-				break;
-			}
-			System.out.printf("%d월은 %d일까지 있습니다.\n", monthArr[i], lastdayArr[i]);
-		}
-
-		sc.close();
+		return lastday;
 	}
-
 }
