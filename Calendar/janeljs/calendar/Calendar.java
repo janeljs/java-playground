871d3c67cbb328966a36128e787e309a93eb68e4
Jisun Lim <jisunlim818@gmail.com>
2020-12-03 19:38:52 +0900

    Calendar.java 윤년 기능 추가

--- Calendar/janeljs/calendar/Calendar.java
+++ Calendar/janeljs/calendar/Calendar.java
@@ -1,49 +1,45 @@
 package janeljs.calendar;
 
-import java.util.Scanner;
-
 public class Calendar {
 
 	public static void printCalendar(int year, int month) {
 		System.out.printf("     <<%4d년 %d월>>\n", year, month);
 		System.out.println(" SU MO TU WE TH FR SA");
-		System.out.println("--------------------");
+		System.out.println(" --------------------");
 
-		int temp = 0;
-		for (int i = 1; i <= 5; i++) {
-			for (int j = 1; j <= 7; j++) {
-				int result = temp + j;
-
-				if (month == 2 && result > 28) {
-					break;
-				}
-				if (month % 2 == 0 && result > 30) {
-					break;
-				}
-				if (result > 31) {
-					break;
-				}
-				System.out.printf("%3d", result);
-
-				if (j == 7) {
-					temp = i * j;
-				}
+		int lastDay = getLastDay(year, month);
+		for (int i = 1; i <= lastDay; i++) {
+			System.out.printf("%3d", i);
+			if (i % 7 == 0) {
+				System.out.println();
 			}
-			System.out.println();
 		}
+		System.out.println();
 	}
 
-	public static int getLastDay(int month) {
+	public static int getLastDay(int year, int month) {
 		int lastday = 0;
-		if (month != 2) {
-			if (month % 2 == 0)
+
+		if (month % 2 == 0) {
+			if (month != 2) {
 				lastday = 30;
-			else {
-				lastday = 31;
+			} else if (month == 2) {
+				if (year % 4 == 0) {
+					if (year % 100 == 0) {
+						if (year % 400 == 0) {
+							lastday = 29;
+						} else {
+							lastday = 28;
+						}
+					} else {
+						lastday = 29;
+					}
+				}
 			}
 		} else {
-			lastday = 28;
+			lastday = 31;
 		}
+
 		return lastday;
 	}
 }
