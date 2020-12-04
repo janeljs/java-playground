b1692af541e04e8d5eb72308487aea1525f562fa
Jisun Lim <jisunlim818@gmail.com>
2020-12-04 22:13:41 +0900

    update PlanItem.java

--- Calendar/janeljs/calendar/PlanItem.java
+++ Calendar/janeljs/calendar/PlanItem.java
@@ -1,55 +1,57 @@
 package janeljs.calendar;
 
-import java.text.ParseException;
+
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Scanner;
 
 public class PlanItem {
-	HashMap<Date, PlanItem> planSave;
-	public PlanItem() {
-		 planSave = new HashMap<>();
+
+	String plan;
+	String location;
+	String guest;
+	
+	public PlanItem() {}
+
+	public PlanItem(String plan, String location, String guest) {
+		this.plan = plan;
+		this.location = location;
+		this.guest = guest;
 	}
 
-	
-	public Date planDate(Scanner sc) {
-		System.out.println("📆 Enter a date (yyyy-MM-dd).");
-		System.out.print("DATE>");
-		String strDate = sc.nextLine();
-		Date date = null;
-		try {
-			date = new SimpleDateFormat("yyyy-MM-dd").parse(strDate);
-			planSave.put(date, null);
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-		return date;
-	}
-	
-	
 	public void planItem(Scanner sc, Date date) {
 		System.out.println("✔️ Add Event");
 		System.out.print("TASK>");
-		String todoItem = sc.nextLine();
+		plan = sc.nextLine();
+
 		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
 		String format = formatter.format(date);
-		System.out.println("📆 " + format + " : " + todoItem + "\n" + "Successfully Registered \n");
+		System.out.println("📆 " + format + " : " + plan + "\n" + "Successfully Registered \n");
 	}
 
-	public void planPlace(Scanner sc, String date) {
+	public void planPlace(Scanner sc, Date date) {
 		System.out.println("🗺️ Add location");
-		System.out.println("LOCATION>");
+		System.out.print("LOCATION>");
+		location = sc.nextLine();
+
 	}
 
-	public void planGuest(Scanner sc, String date) {
+	public void planGuest(Scanner sc, Date date) {
+		ArrayList<String> guests = new ArrayList<>();
 		System.out.println("🗺️ Add guests (press q to quit)");
+		String guest;
+		
 		while (true) {
-			System.out.println("GUESTS>");
-			String guest = sc.nextLine();
+			System.out.print("GUEST>");
+			guest = sc.nextLine();
+			
 			if (guest.equals("q")) {
+				System.out.println("GUESTS: " + guests);
+				System.out.println();
 				break;
 			}
+			guests.add(guest);
 		}
 
 	}
